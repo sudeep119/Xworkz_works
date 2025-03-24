883a3c4239a30465c819e8ac6fefe250093ff397
Sudeep <sudeepks1005@gmail.com>
2025-03-24 08:40:23 +0530

    instance

---
+++ DailyQuest/City.java
@@ -0,0 +1,12 @@
+public class City {
+
+
+        String[] Ward={
+                "floor","place"
+        };
+        public City(){
+            for(String ward: Ward){
+                System.out.println(ward);
+            }
+        }
+    }
